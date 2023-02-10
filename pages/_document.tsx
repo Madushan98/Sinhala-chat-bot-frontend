a38a6f8bf05b9a1248ec069a37745fb87262ba26
PREMINDA <premindabandara0777@gmail.com>
2023-02-10 14:08:40 +0530

    First commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html  lang="en">
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400;1,500&display=swap" rel="stylesheet"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
